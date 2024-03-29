bbae88443718d992fbaf4891a1517c58207bf744
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-29 18:53:48 +1100

    adding link between page

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,12 +1,12 @@
 import React, { ReactNode } from 'react'
 import MainHeader from './layout-sections/MainHeader'
-import WikiPage from './layout-sections/WikiPage'
+import HomePage from '@/app/page'
 
 const MainLayout = ({children}) => {
   return (
     <div className='min-h-screen'>
       <MainHeader />
-      <WikiPage />
+      <main>{children}</main>
     </div>
   )
 }
--- src/layout/layout-sections/MainHeader.tsx
+++ src/layout/layout-sections/MainHeader.tsx
@@ -2,6 +2,7 @@
 
 import MainHeaderButton from '@/components/MainHeaderButton';
 import WhiteReactIcon from '@/utils/WhiteReactIcon';
+import Link from 'next/link';
 import React from 'react'
 import { LuGamepad2 } from "react-icons/lu";
 
@@ -9,14 +10,16 @@ const MainHeader = () => {
   return (
     <div className='w-full bg-secondary-dark py-6 px-4 flex flex-row'>
       <div className='flex items-center justify-start mx-4'>
-        <WhiteReactIcon>
-          <LuGamepad2 className='cursor-pointer'/>
-        </WhiteReactIcon>
+        <Link href={`/`}>
+          <WhiteReactIcon>
+            <LuGamepad2 className='cursor-pointer'/>
+          </WhiteReactIcon>
+        </Link>
       </div>
       <div className='flex flex-col flex-grow items-start mx-4 justify-center'>
         <h1 className='text-white font-bold text-2xl mb-3'>Little Knight Wiki</h1>
         <div className='flex flex-row justify-between items-center gap-6'>
-          <MainHeaderButton name='Explore' />
+          <MainHeaderButton name='Explore' subUrl='Wiki'/>
           <MainHeaderButton name='Character' />
           <MainHeaderButton name='Location' />
           <MainHeaderButton name='Lore' />
