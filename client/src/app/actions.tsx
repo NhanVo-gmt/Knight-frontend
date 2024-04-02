09d0aedd03721327e14db951880be2b476126918
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-04-03 01:18:06 +1100

    adding subscribe action for button

--- client/src/app/actions.tsx
+++ client/src/app/actions.tsx
@@ -2,17 +2,31 @@
 
 const subscribeUrl = "http://localhost:8080/subscribe";
 
+
+
 export async function subscribe(prev: any, formData: FormData) {
-    
-    await fetch(subscribeUrl, {
-        body: JSON.stringify({
-            email: formData.get('email')
-        }),
+    try {
 
-        headers: {
-            'Content-Type': 'application/json',
-        },
+        const email = formData.get("email");
+        const firstname = formData.get("firstname");
+        const lastname = formData.get("lastname");
 
-        method: 'POST',
-    });
+        const data = {
+            email: email,
+            firstname: firstname ? firstname : "",
+            lastname: lastname ? lastname : "",
+        };
+
+        const res = await fetch(subscribeUrl, {
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'POST',
+        });
+        console.log(res);
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
