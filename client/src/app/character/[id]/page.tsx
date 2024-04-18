6d4b278913b5802684024186f22bbf6f1a9b83e8
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-04-18 20:45:05 +1000

    adding character page

--- client/src/app/character/[id]/page.tsx
+++ client/src/app/character/[id]/page.tsx
@@ -1,18 +1,33 @@
 // /character/[id].tsx
 
+import { characters } from "@/data/CharacterPageData";
+
 type CharacterPageType = {
-    id: number
-    name: string,
-    params: any
-}
+  id: number;
+  params: any;
+};
 
-export default function CharacterPage({params} : CharacterPageType) {
+export default function CharacterPage({ params }: CharacterPageType) {
+  const character = characters.find((item) => item.id == params.id);
 
-    return (
-        <div>
-            <h1>Character Details</h1>
-            <p>Character ID: {params.id}</p>
-            {/* Render other details of the character based on the ID */}
+  return (
+    <div className="bg-secondary-dark-hover">
+        <div className='flex flex-row p-20 gap-x-10'>
+        <div className="basis-1/2">
+            <div className="text-5xl my-10 font-bold">{character?.name}</div>
+            <div className="text-2xl">{character?.description}</div>
         </div>
-    );
+        <div className="flex flex-grow basis-1/2 ">
+            <img src={character?.imgUrl} alt={character?.name} className="h-[600px]"/>
+            <div className="absolute h-0 overflow-hidden bg-black opacity-0 bottom-0 ">
+            <div className="relative mt-10 opacity-100">
+                <div className="text-bold text-6xl text-center">
+                {character?.name}
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+  );
 }
